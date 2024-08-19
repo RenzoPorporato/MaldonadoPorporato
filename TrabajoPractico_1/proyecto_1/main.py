02d946d475b0ba825c0d78cf2bd0ba09d03cadc8
Nestor <albornoz451@hotmail.com>
2024-08-19 14:44:01 -0300

    Cambio de nombre en config.py
    Nose si usar server o __name__

--- TrabajoPractico_1/proyecto_1/main.py
+++ TrabajoPractico_1/proyecto_1/main.py
@@ -1,1 +1,12 @@
 # Aplicación principal
+from modules.Config import app
+
+@app.route("/")
+def prueba_saludo():
+     pass
+
+
+if __name__ == "__main__":      
+     app.run(debug=True)
+
+#Esto va al final, asumo que es para correr la aplicacion
--- TrabajoPractico_1/proyecto_1/modules/Config.py
+++ TrabajoPractico_1/proyecto_1/modules/Config.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app=Flask("server") #Se pasa el nombre de la aplicacion para que flask sepa donde buscar plantillas y recursos estaticos
+app=Flask("__name__") #Se pasa el nombre de la aplicacion para que flask sepa donde buscar plantillas y recursos estaticos
 
 
 
