28876ca9e27ab5f25f309e230e8ca30edca48f61
Nestor <albornoz451@hotmail.com>
2024-08-19 11:58:48 -0300

    primer update dominio.py
    Creo dos funciones, una para crear una lista que contiene sublistas, cada sublista tiene una frase y su pelicula. Luego creo otra funcion donde se ordena esa lista alfabeticamente

---
+++ TrabajoPractico_1/proyecto_1/modules/Dominio.py
@@ -0,0 +1,15 @@
+def extraer_datos (nombre_archivo):
+    with open (nombre_archivo,"r") as archi:
+    datos=list()
+    for i in archi:
+        fras,peli=i.split(";")
+        datos.append([fras.strip().lower(),peli.strip().lower()])
+    return datos
+#Saco la informacion del archivo
+
+
+def ordenar_datos (matriz):
+    datos_ordenados=sorted(datos,key=lambda x:x[1])
+    return datos_ordenados
+#ordeno alfabeticamente la matriz
+
