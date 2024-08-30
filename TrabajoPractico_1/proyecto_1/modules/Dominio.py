182890e38dcab13c0423972b87aa8dc1d04c0968
Nestor <albornoz451@hotmail.com>
2024-08-30 15:33:43 -0300

    Update dominio.py
    Creo una funcion para obtener una pelicula correcta y 2 incorrectas

--- TrabajoPractico_1/proyecto_1/modules/Dominio.py
+++ TrabajoPractico_1/proyecto_1/modules/Dominio.py
@@ -1,3 +1,5 @@
+import random 
+
 def extraer_datos (nombre_archivo):
     with open (nombre_archivo,"r") as archi:
         datos=list()
@@ -7,15 +9,37 @@ def extraer_datos (nombre_archivo):
     return datos
 #Saco la informacion del archivo
 
-
 def ordenar_datos (matriz):
     datos_ordenados=sorted(matriz,key=lambda x:x[1])
     return datos_ordenados
 #ordeno alfabeticamente la matriz
 
 def asociar_frases (matriz):
-    frases={}
+    frases_peliculas={}
     for i in matriz:
         frases[i[0]]=i[1]
-    return frases
-#Se crea un diccionario donde cada frase tiene asociada una pelicula
+    return frases_peliculas
+#Se crea un diccionario donde cada frase tiene asociada una pelicula#
+
+#De aca para abajo es para obtener una pelicula correcta mezclada con incorrectas
+frases_peliculas=asociar_frases(extraer_datos("NOMBRE ARCHIVO"))
+frases=list(frases_peliculas.keys())#Arma una lsita con las frases sacadas del diccionario
+frase_seleccionada= random.choice(frases)#Saca una frase al azar
+pelicula_correcta= frases_peliculas[frase_seleccionada]#Guarda la pelicula asociada a esa frase
+
+def trivia (pelicula_correcta, peliculas_ordenadas):    
+    opciones=[pelicula_correcta]#Crea una lista empezando con la pelicula correcta
+    while len(opciones)<3:
+        pelicula=random.choice(peliculas_ordenadas)
+        if pelicula != pelicula_correcta and pelicula not in opciones:
+            opciones.append(pelicula)
+    random.shuffle(opciones)
+    return opciones
+
+
+
+
+
+    
+    
+    
